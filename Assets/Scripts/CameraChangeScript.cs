96f0fcce4664cb64ab16ab06e0aac464ccff959b
juhalallukka <46740592+juhalallukka@users.noreply.github.com>
2025-04-07 14:41:28 +0300

    kameran vaihto2
    alt vaihtaa

---
+++ Assets/Scripts/CameraChangeScript.cs
@@ -0,0 +1,73 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChangeScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //public Camera camCar1;
+    //public Camera camCar2;
+
+    //public Camera camDrone1;
+
+    public List<Camera> camList;
+    private int currentIndex = 0;
+    void Start()
+    {
+
+        //camList.Add(camCar1);
+        //camList.Add(camCar2);
+        //camList.Add(camDrone1);
+
+
+        //camCar1.enabled = true;
+        //camDrone1.enabled = false;
+        EnableCamera(0);
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftAlt) == true)
+        {
+            RotateCameraList();
+            //camCar1.enabled = !camCar1.enabled;
+            //camDrone1.enabled = !camDrone1.enabled;
+
+
+
+        }
+    }
+
+    void EnableCamera(int index)
+    {
+        
+        for (int i = 0; i < camList.Count; i++)
+        {
+            camList[i].enabled = false;
+        }
+
+        camList[index].enabled = true;
+        
+    }
+
+    void RotateCameraList()
+    {
+        
+        if (currentIndex < camList.Count - 1)
+        {
+            currentIndex++;
+        }
+        else
+        {
+            currentIndex = 0;
+        }
+
+        EnableCamera(currentIndex);
+    }
+
+}
