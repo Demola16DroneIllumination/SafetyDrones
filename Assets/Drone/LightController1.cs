c6dcf90343493801365079072fcb76b0735acaf4
Heikki Pätsi <heikki.patsi@outlook.com>
2025-04-10 04:09:17 +0300

    Projector light switch
    Added button to turn projector on/off and go through a list of traffic signs.

---
+++ Assets/Drone/LightController1.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public Light stoplight;
+
+        public void ToggleStoplight()
+    {
+        if (stoplight != null)
+        {
+            stoplight.enabled = !stoplight.enabled;
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ToggleStoplight();
+        }
+    }
+}
+
+
